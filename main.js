b092054b2156d3fac4c13bdb366caee8350f17d8
YogiPaturu <ypaturu@gmail.com>
2021-10-13 21:47:31 -0500

    done for the night

--- main.js
+++ main.js
@@ -2,53 +2,79 @@
 
 // mathProblemGenerator
 function mathProblemGenerator() {
-    // Array of math operations
-    const operators = [
-        {
-            operation: '+',
-            method: function(a,b){return a + b}
-        },
-        {
-            operation: '-',
-            method: function(a,b){return a - b}
-        },
-        {
-            operation: '*',
-            method: function(a,b){return a * b}
-        }
-    ]
-    
-    // Two random numbers between 0 and 100
-    const randomA = Math.round(Math.random()*100);
-    const randomB = Math.round(Math.random()*100);
+  // Array of math operations
+  const operators = [
+    {
+      operation: "+",
+      method: function (a, b) {
+        return a + b;
+      },
+    },
+    {
+      operation: "-",
+      method: function (a, b) {
+        return a - b;
+      },
+    },
+    {
+      operation: "*",
+      method: function (a, b) {
+        return a * b;
+      },
+    },
+  ];
 
-    // Random index to select random math operation array
-    const randomMathFunc = Math.round(Math.random()*(operators.length - 1))
-    
-    // Pass through random numbers into random operation array method
-    const answer = Math.round(operators[randomMathFunc].method(randomA,randomB));
-    const mathOperation = [randomA, operators[randomMathFunc].operation, randomB, answer];
-    return mathOperation
+  // Two random numbers between 0 and 100
+  const randomA = Math.round(Math.random() * 100);
+  const randomB = Math.round(Math.random() * 100);
+
+  // Random index to select random math operation array
+  const randomMathFunc = Math.round(Math.random() * (operators.length - 1));
+
+  // Pass through random numbers into random operation array method
+  const answer = Math.round(operators[randomMathFunc].method(randomA, randomB));
+  const mathOperation = [
+    randomA,
+    operators[randomMathFunc].operation,
+    randomB,
+    answer,
+  ];
+  return mathOperation;
 }
 console.log(mathProblemGenerator());
 
 // blockWebpage - overlay image of adi
-function blockWebpage(body) {
-  const adiImage = document.createElement('img');
-  adiImage.setAttribute('class', 'adiImage');
-  adiImage.style.height = '100vh';
-  adiImage.style.width = '100wh';
-  adiImage.style.position = 'absolute';
+function blockWebpage() {
+  const adiImage = document.createElement("img");
+  adiImage.setAttribute("class", "adiImage");
+  adiImage.setAttribute("src", "https://imgur.com/1KO5bOB.jpeg");
+  adiImage.style.height = "200vh";
+  adiImage.style.width = "200wh";
+  adiImage.style.position = "absolute";
   adiImage.style.top = 0;
   adiImage.style.left = 0;
   adiImage.style.zIndex = 100;
-  
+
+  // const sound = document.createElement("audio");
+  // sound.autoplay = "true";
+  // sound.id = "audio-player";
+  // sound.controls = "controls";
+  // sound.src = "/banjo.mp3";
+  // sound.type = "audio/mp3";
+  // sound.load();
+  // sound.addEventListener("load", function() { 
+  //   sound.play(); 
+  // }, true);
+
+
+  const body = document.querySelector("body");
   body.appendChild(adiImage);
+  console.log(body);
 }
 
 // unblockWebpage - if solution to mathProblem is correct, remove overlayed image
 function unblockWebpage(body) {
-  const adiImage = document.querySelector('.adiImage');
+  const adiImage = document.querySelector(".adiImage");
   body.removeChild(adiImage);
 }
 
@@ -57,24 +83,23 @@ function unblockWebpage(body) {
 [ ] after solving, reset the timer
 [ ] when timer runs out, invoke popup
 */
-const docBody = document.querySelector('body');
 
-function pomodoroTimer(body){
-  blockWebpage(body);
-  
-  const mathArray = mathProblemGenerator();
-  const mathPopup = document.querySelector('')
-  const mathProblem = document.createElement('div');
-  mathProblem.style.zIndex = 101;
+function pomodoroTimer() {
+  blockWebpage();
 
-  mathProblem.innerText = `${mathArray[0]} ${mathArray[1]} ${mathArray[2]}`;
-  // popup.appendChild(mathProblem);
-  body.appendChild(mathProblem);
+  // const mathArray = mathProblemGenerator();
+  // const mathPopup = document.querySelector('')
+  // const mathProblem = document.createElement('div');
+  // mathProblem.style.zIndex = 101;
 
+  // mathProblem.innerText = `${mathArray[0]} ${mathArray[1]} ${mathArray[2]}`;
+  // // popup.appendChild(mathProblem);
+  // body.appendChild(mathProblem);
+  console.log("loaded");
 
   // document.getElementById("#answer");
 
-  setTimeout(pomodoroTimer, 100000)
+  setTimeout(pomodoroTimer, 100000);
 }
 // let username = document.getElementById("username").value;
 // let password = document.getElementById("password").value;
@@ -87,8 +112,7 @@ function pomodoroTimer(body){
 //     window.alert('no no no');
 // }
 
-
 // browser.runtime.onInstalled.addListener(pomodoroTimer)
-// blockWebpage -> mathProblemGenerator -> popup -> unblockWebpage -> pomodoroTimer -> 
+// blockWebpage -> mathProblemGenerator -> popup -> unblockWebpage -> pomodoroTimer ->
 
-document.addEventListener('DOMContentLoaded', pomodoroTimer(docBody))
+document.addEventListener("DOMContentLoaded", pomodoroTimer());
