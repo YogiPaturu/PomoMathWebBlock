74685574a50782c3ccc7abdf436ac591601e849f
YogiPaturu <ypaturu@gmail.com>
2021-10-14 13:31:30 -0500

    FaceBlock

--- main.js
+++ main.js
@@ -1,53 +1,10 @@
-// eventHandler for when extension loads
 
-// mathProblemGenerator
-function mathProblemGenerator() {
-  // Array of math operations
-  const operators = [
-    {
-      operation: "+",
-      method: function (a, b) {
-        return a + b;
-      },
-    },
-    {
-      operation: "-",
-      method: function (a, b) {
-        return a - b;
-      },
-    },
-    {
-      operation: "*",
-      method: function (a, b) {
-        return a * b;
-      },
-    },
-  ];
-
-  // Two random numbers between 0 and 100
-  const randomA = Math.round(Math.random() * 100);
-  const randomB = Math.round(Math.random() * 100);
-
-  // Random index to select random math operation array
-  const randomMathFunc = Math.round(Math.random() * (operators.length - 1));
-
-  // Pass through random numbers into random operation array method
-  const answer = Math.round(operators[randomMathFunc].method(randomA, randomB));
-  const mathOperation = [
-    randomA,
-    operators[randomMathFunc].operation,
-    randomB,
-    answer,
-  ];
-  return mathOperation;
-}
-console.log(mathProblemGenerator());
 
 // blockWebpage - overlay image of adi
 function blockWebpage() {
   const adiImage = document.createElement("img");
   adiImage.setAttribute("class", "adiImage");
-  adiImage.setAttribute("src", "https://imgur.com/1KO5bOB.jpeg");
+  adiImage.setAttribute("src", "https://www.ballaratyoga.com.au/wp-content/uploads/2017/12/wooden-block-3.jpg");
   adiImage.style.height = "200vh";
   adiImage.style.width = "200wh";
   adiImage.style.position = "absolute";
@@ -70,49 +27,69 @@ function blockWebpage() {
   const body = document.querySelector("body");
   body.appendChild(adiImage);
   console.log(body);
+  console.log('blockWebpage executed')
 }
 
-// unblockWebpage - if solution to mathProblem is correct, remove overlayed image
-function unblockWebpage(body) {
-  const adiImage = document.querySelector(".adiImage");
-  body.removeChild(adiImage);
-}
+document.addEventListener("DOMContentLoaded", blockWebpage())
 
-/* timer:
-[ ] invoke popup upon installation
-[ ] after solving, reset the timer
-[ ] when timer runs out, invoke popup
-*/
+// document.addEventListener("click", () => {
+//   const myAudio = new Audio(chrome.runtime.getURL("./knock.mp3"));
+//   myAudio.controls = 'controls';
+//   myAudio.play();
+// });
 
-function pomodoroTimer() {
-  blockWebpage();
 
-  // const mathArray = mathProblemGenerator();
-  // const mathPopup = document.querySelector('')
-  // const mathProblem = document.createElement('div');
-  // mathProblem.style.zIndex = 101;
 
-  // mathProblem.innerText = `${mathArray[0]} ${mathArray[1]} ${mathArray[2]}`;
-  // // popup.appendChild(mathProblem);
-  // body.appendChild(mathProblem);
-  console.log("loaded");
 
-  // document.getElementById("#answer");
 
-  setTimeout(pomodoroTimer, 100000);
-}
-// let username = document.getElementById("username").value;
-// let password = document.getElementById("password").value;
+// ----------------------
+// eventHandler for when extension loads
 
-// if(username === "yogi.jpeg.exe" && password === "123") {
-//     console.log("Patrick Constantine");
-//     window.location.assign("https://www.instagram.com");
+// mathProblemGenerator
+// function mathProblemGenerator() {
+//   // Array of math operations
+//   const operators = [
+//     {
+//       operation: "+",
+//       method: function (a, b) {
+//         return a + b;
+//       },
+//     },
+//     {
+//       operation: "-",
+//       method: function (a, b) {
+//         return a - b;
+//       },
+//     },
+//     {
+//       operation: "*",
+//       method: function (a, b) {
+//         return a * b;
+//       },
+//     },
+//   ];
+
+//   // Two random numbers between 0 and 100
+//   const randomA = Math.round(Math.random() * 100);
+//   const randomB = Math.round(Math.random() * 100);
+
+//   // Random index to select random math operation array
+//   const randomMathFunc = Math.round(Math.random() * (operators.length - 1));
+
+//   // Pass through random numbers into random operation array method
+//   const answer = Math.round(operators[randomMathFunc].method(randomA, randomB));
+//   const mathOperation = [
+//     randomA,
+//     operators[randomMathFunc].operation,
+//     randomB,
+//     answer,
+//   ];
+//   return mathOperation;
 // }
-// else {
-//     window.alert('no no no');
+// console.log(mathProblemGenerator());
+
+// // unblockWebpage - if solution to mathProblem is correct, remove overlayed image
+// function unblockWebpage(body) {
+//   const adiImage = document.querySelector(".adiImage");
+//   body.removeChild(adiImage);
 // }
-
-// browser.runtime.onInstalled.addListener(pomodoroTimer)
-// blockWebpage -> mathProblemGenerator -> popup -> unblockWebpage -> pomodoroTimer ->
-
-document.addEventListener("DOMContentLoaded", pomodoroTimer());
